7cccb51002882c2c04d5427ff97506155b2d411e
Yulian Allan Muñoz Todd <yulian.todd.dev@skiff.com>
2023-10-17 20:14:57 +0000

    Update gatewayServer.ts

--- src/gatewayServer.ts
+++ src/gatewayServer.ts
@@ -3,7 +3,6 @@ import http from "http";
 import * as mongoose from "mongoose";
 import { config } from "./config/config";
 const router = express();
-const port = 3000;
 import Logging from "./library/Logging";
 // Ruta de prueba
 router.get("/", (req, res) => {
@@ -34,3 +33,44 @@ const StartServer = () => {
   router.use(express.urlencoded({ extended: true }));
   router.use(express.json());
 };
+/** Rules */
+router.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+  );
+
+  if (req.method == "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+
+  next();
+});
+
+/** Routes */
+//router.use('/authors', authorRoutes);
+//router.use('/books', bookRoutes);
+
+/** Healthcheck */
+router.get("/ping", (req, res, next) =>
+  res.status(200).json({ hello: "world" }),
+);
+
+/** Error handling */
+router.use((req, res, next) => {
+  const error = new Error("Not found");
+
+  Logging.error(error);
+
+  res.status(404).json({
+    message: error.message,
+  });
+});
+
+http
+  .createServer(router)
+  .listen(config.server.port, () =>
+    Logging.info(`Server is running on port ${config.server.port}`),
+  );
